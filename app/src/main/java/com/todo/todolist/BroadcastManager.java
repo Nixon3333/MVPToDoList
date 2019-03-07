3c8ee3e267ca618fcc4158f0877356a824ce65d0
Nikita Drygin <nixon3333@gmail.com>
2019-03-07 17:38:36 +0700

    Added notification

--- app/src/main/java/com/todo/todolist/BroadcastManager.java
+++ app/src/main/java/com/todo/todolist/BroadcastManager.java
@@ -1,8 +1,13 @@
 package com.todo.todolist;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -15,6 +20,8 @@ import java.util.List;
 public class BroadcastManager extends BroadcastReceiver {
 
     Model model;
+    NotificationCompat.Builder notifBuilder;
+
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -22,6 +29,10 @@ public class BroadcastManager extends BroadcastReceiver {
         model = new Model();
         List<Task> dates = model.loadTasks(context);
         StringBuilder sb = new StringBuilder();
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
         for (Task task : dates) {
             //Log.d("Dates", date);
             //Log.d("Dates", String.valueOf(dateToMillis(date)));
@@ -30,8 +41,24 @@ public class BroadcastManager extends BroadcastReceiver {
             if (dateToMillis(task.getDate()) - System.currentTimeMillis() < 50400000) {
 
                 sb.append(task.getDate()).append(" ");
+
             }
         }
+        notifBuilder = new NotificationCompat.Builder(context)
+        .setSmallIcon(R.drawable.ic_launcher_foreground)
+        .setContentTitle("My not")
+        .setContentText(sb)
+        .setChannelId("Channel");
+        NotificationChannel mChannel = null;
+        int importance = NotificationManager.IMPORTANCE_HIGH;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            mChannel = new NotificationChannel("Channel", "MyChannel", importance);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            mNotificationManager.createNotificationChannel(mChannel);
+        }
+        mNotificationManager.notify(1, notifBuilder.build());
         Toast.makeText(context, sb.toString(), Toast.LENGTH_LONG).show();
 
     }
